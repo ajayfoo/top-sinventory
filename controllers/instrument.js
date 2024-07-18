1e1d9486883a0453949786a89403b5f76e195b2e
Ajay K <113184147+ajayfoo@users.noreply.github.com>
2024-07-18 11:31:12 +0530

    feat: add update_instrument_form.ejs

--- controllers/instrument.js
+++ controllers/instrument.js
@@ -11,4 +11,23 @@ const render = (req, res, next) => {
     ...instrument,
   });
 };
-export { create, render };
+
+const renderUpdateForm = (req, res, next) => {
+  const targetInstrument = instruments.find((i) => i._id === req.params.id);
+  res.render("update_instrument_form", {
+    title: "Update " + targetInstrument.name,
+    ...targetInstrument,
+  });
+};
+
+const update = (req, res, next) => {
+  const targetInstrument = instruments.find((i) => i._id === req.body._id);
+  const { name, description, price, count } = req.body;
+  targetInstrument.name = name;
+  targetInstrument.description = description;
+  targetInstrument.price = price;
+  targetInstrument.count = count;
+  res.redirect("../../");
+};
+
+export { create, render, renderUpdateForm, update };
--- routes/instrument.js
+++ routes/instrument.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { render } from "../controllers/instrument.js";
+import { render, renderUpdateForm, update } from "../controllers/instrument.js";
 
 const router = Router();
 
 router.get("/:id", render);
+router.get("/:id/update", renderUpdateForm);
+router.post("/:id/update", update);
 
 export default router;
